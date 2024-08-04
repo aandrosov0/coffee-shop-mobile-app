7098fcac492e488a0915934a74b2f8144c83d221
Anton Androsov <aandrosov338@gmail.com>
2024-08-04 13:48:37 +0300

    feat: update both compileSdk and targetSdk up to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,13 +10,13 @@ kotlin {
 android {
     namespace = "com.example.app"
 
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.app"
 
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
 
         versionCode = 1
         versionName = "1.0.0"
